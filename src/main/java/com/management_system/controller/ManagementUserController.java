70a8bf97213d2935f569eb92490a8b0ef4028cfa
Yang_glb <Yang_glb@jiulvxing.com>
2019-05-22 15:46:18 +0800

    d

--- src/main/java/com/management_system/controller/ManagementUserController.java
+++ src/main/java/com/management_system/controller/ManagementUserController.java
@@ -9,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.management_system.bean.Msg;
 import com.management_system.service.ManagementUserService;
 
 @Controller
