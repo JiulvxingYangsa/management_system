3b113c41bddc6ccbc347ac9ad175119c38b27809
Yang_glb <Yang_glb@jiulvxing.com>
2019-05-22 17:28:09 +0800

    annotation service

--- src/main/java/com/management_system/service/LoginService.java
+++ src/main/java/com/management_system/service/LoginService.java
@@ -4,5 +4,11 @@ import com.management_system.bean.ManagementUser;
 
 public interface LoginService {
 
+	/**
+	 * 查询管理员账号密码
+	 * @param uUser
+	 * @param uPassword
+	 * @return
+	 */
 	public ManagementUser getByUser(String uUser,String uPassword);
 }
--- src/main/java/com/management_system/service/ManagementUserService.java
+++ src/main/java/com/management_system/service/ManagementUserService.java
@@ -3,5 +3,15 @@ package com.management_system.service;
 
 public interface ManagementUserService {
 
+	/**
+	 * 添加管理员账户account
+	 * @param uName
+	 * @param uAge
+	 * @param uGender
+	 * @param uNumber
+	 * @param uUser
+	 * @param uPassword
+	 * @return
+	 */
 	public int insertManagementUser(String uName,Integer uAge,String uGender,String uNumber,String uUser,String uPassword);
 }
